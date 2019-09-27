f59bf5de99ee681c207753ae35ca3fdcec970737
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-27 16:17:20 -0700

    Update CommentFragment.java

--- CommentFragment.java
+++ CommentFragment.java
@@ -1,9 +1,78 @@
+public class CommentFragment extends Fragment {
+   private GridView gridView;
+
+   OnItemSelectListener callback;
+   // Container Activity must implement this interface
+   public interface OnItemSelectListener {
+       public void onCommentSelected(int position);
+   }
+
+   @Override
+   public void onAttach(Context context) {
+       super.onAttach(context);
+       try {
+           callback = (OnItemSelectListener) context;
+       } catch (ClassCastException e) {
+           //do something
+       }
+   }
+
+  public static CommentFragment newInstance(int position) {
+  
+   Bundle args = new Bundle();
+   args.putInt("position", position);
+   CommentFragment fragment = new CommentFragment();
+   fragment.setArguments(args);
+   return fragment;
+}
+
+
+
+@Override
+public void onResume() {
+   super.onResume();
+   Bundle bundle = getArguments();
+   if (bundle != null) {
+       final int position = (int) bundle.get("position");
+       gridView.post(new Runnable() {
+           @Override
+           public void run() {
+               onItemSelected(position);
+           }
+       });
+   }
+}
+
+
 @Override
 public View onCreateView(LayoutInflater inflater, ViewGroup container,
                         Bundle savedInstanceState) {
    // Inflate the layout for this fragment
    View view = inflater.inflate(R.layout.fragment_comment, container, false);
-   GridView gridView = (GridView) view.findViewById(R.id.comment_grid);
+   gridView = (GridView) view.findViewById(R.id.comment_grid);
    gridView.setAdapter(new EventAdapter(getActivity()));
+  
+   // add click listen to grid item
+   gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+       @Override
+       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+           callback.onCommentSelected(position);
+       }
+   });
    return view;
 }
+
+
+   // Change background color if the item is selected
+   public void onItemSelected(int position){
+       for (int i = 0; i < gridView.getChildCount(); i++){
+           if (position == i) {
+               gridView.getChildAt(i).setBackgroundColor(Color.BLUE);
+           } else {
+               gridView.getChildAt(i).setBackgroundColor(Color.parseColor("#FAFAFA"));
+           }
+       }
+   }
+
+}
+
