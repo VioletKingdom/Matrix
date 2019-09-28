3ab51fe8e9d9d0f4c7297bf8911e10131d3d3124
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-27 18:36:52 -0700

    Update MyFirebaseMessagingService.java

--- MyFirebaseMessagingService.java
+++ MyFirebaseMessagingService.java
@@ -1,30 +1,70 @@
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-   private static final String TAG = "FirebaseService";
-   private Context mContext;
+/**
+* Create and show a simple notification containing the received FCM message.
+*/
+/**
+* Create and show a simple notification containing the received FCM message.
+*/
+private void sendNotification(String title, String body) {
+private void sendNotification(RemoteMessage remoteMessage) {
+   String type = remoteMessage.getData().get("type");
+   String description = remoteMessage.getData().get("description");
+   Bitmap icon =  BitmapFactory.decodeResource(mContext.getResources(),
+           Config.trafficMap.get(type));
 
-   @Override
-   public void onNewToken(String token) {
-       Log.d(TAG, "Refreshed token: " + token);
-       // If you want to send messages to this application instance or
-       // manage this apps subscriptions on the server side, send the
-       // Instance ID token to your app server.
+   Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+   Intent intent = new Intent(getApplicationContext(), ControlPanel.class);
+   intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+   PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+   NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+   String NOTIFICATION_CHANNEL_ID = "101";
+
+   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+       NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_HIGH);
+
+       //Configure Notification Channel
+       notificationChannel.setDescription("Matrix Notfication");
+       notificationChannel.enableLights(true);
+       notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+       notificationChannel.enableVibration(true);
+
+       notificationManager.createNotificationChannel(notificationChannel);
    }
 
-   @Override
-   public void onMessageReceived(RemoteMessage remoteMessage) {
-       // TODO(developer): Handle FCM messages here.
-       Log.d(TAG, "From: " + remoteMessage.getFrom());
-       if (mContext == null) {
-           mContext = getBaseContext();
-       }
-       // Check if message contains a data payload.
-       if (remoteMessage.getData().size() > 0) {
-           Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-       }
+   NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+           .setSmallIcon(R.drawable.boy)
+           .setContentTitle(type)
+           .setAutoCancel(true)
+           .setSound(defaultSound)
+           .setLargeIcon(icon)
+           .setContentText(description)
+           .setContentIntent(pendingIntent)
+           .setWhen(System.currentTimeMillis())
+           .setPriority(Notification.PRIORITY_MAX);
 
-       // Check if message contains a notification payload.
-       if (remoteMessage.getNotification() != null) {
-           Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-       }
+   notificationManager.notify(1, notificationBuilder.build());
+}
+
+
+@Override
+public void onMessageReceived(RemoteMessage remoteMessage) {
+   // TODO(developer): Handle FCM messages here.
+   Log.d(TAG, "From: " + remoteMessage.getFrom());
+   if (mContext == null) {
+       mContext = getBaseContext();
    }
+   // Check if message contains a data payload.
+   if (remoteMessage.getData().size() > 0) {
+       Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+   }
+
+   // Check if message contains a notification payload.
+   sendNotification(remoteMessage);
+   if (remoteMessage.getNotification() != null) {
+       Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+   }
+}
+
+
 }
