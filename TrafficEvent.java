e7b1fb4da48ab2c7c614b521d603f49c690f7c2a
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-27 18:35:49 -0700

    Create TrafficEvent.java

---
+++ TrafficEvent.java
@@ -0,0 +1,13 @@
+public class TrafficEvent {
+   private String id;
+   private String event_type;
+   private int event_comment_number;
+   private long event_timestamp;
+   private double event_longitude;
+   private double event_latitude;
+   private String event_reporter_id;
+   private String event_level;
+   private int event_like_number;
+   private String event_description;
+   private String imgUri;
+}
