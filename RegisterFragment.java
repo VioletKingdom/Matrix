a052a30f0c1b586415aae32a4d7bb155d03ecb8e
VioletKingdom <55386758+VioletKingdom@users.noreply.github.com>
2019-09-27 17:33:24 -0700

    Update RegisterFragment.java

--- RegisterFragment.java
+++ RegisterFragment.java
@@ -20,13 +20,60 @@ public class RegisterFragment extends OnBoardingBaseFragment {
        // Inflate the layout for this fragment
        View view = super.onCreateView(inflater, container, savedInstanceState);
        submitButton.setText(getString(R.string.register));
+
+       // register the account to firebase
+       submitButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               final String username = usernameEditText.getText().toString();
+               final String password = passwordEditText.getText().toString();
+
+               database.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(DataSnapshot dataSnapshot) {
+                       if (dataSnapshot.hasChild(username)) {
+                           Toast.makeText(getActivity(), "username is already registered, please change one", Toast.LENGTH_SHORT).show();
+                       } else if (!username.equals("") && !password.equals("")) {
+                           // put username as key to set value
+                           final User user = new User();
+                           user.setUser_account(username);
+                           user.setUser_password(Utils.md5Encryption(password));
+                           user.setUser_timestamp(System.currentTimeMillis());
+                           database.child("user").child(user.getUser_account()).setValue(user);
+                           Toast.makeText(getActivity(), "Successfully registered", Toast.LENGTH_SHORT).show();
+                           goToLogin();
+                       }
+                   }
+
+                   @Override
+                   public void onCancelled(DatabaseError databaseError) {
+
+                   }
+               });
+           }
+       });
+
+       AdView mAdView = (AdView) view.findViewById(R.id.adView);
+       AdRequest adRequest = new AdRequest.Builder().build();
+       mAdView.loadAd(adRequest);
+
        return view;
+   }
+
+   private void goToLogin() {
+       Activity activity = getActivity();
+       if (activity != null && !activity.isFinishing()) {
+           ((OnBoardingActivity)activity).setCurrentPage(0);
+       }
    }
 
    @Override
    protected int getLayout() {
        return R.layout.fragment_register;
    }
+
+}
+
 
 }
 
